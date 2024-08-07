a28b640ebec1741ba8aab90d5eae5ecfef98f7ec
hj <hanguswls@knu.ac.kr>
2024-08-08 08:56:21 +0900

    feat: App.jsx에 route 표시

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Footer from './components/footer/Footer'
 import Login from './routes/Login'
 import useRefresh from './hooks/useRefresh'
 import CreatePost from './routes/CreatePost'
+import UpdateUserInfo from './routes/UpdateUserInfo'
+import ViewUserInfo from './routes/ViewUserInfo'
+import ViewMyUserInfo from './routes/ViewMyUserInfo'
 import useInitializeUserInfo from './hooks/useInitializeUserInfo'
 
 function App() {
@@ -21,6 +24,9 @@ function App() {
         <Route path='/register' element={<Register />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/upload' element={<CreatePost />} />
+        <Route path='/users/:id' element={<ViewUserInfo />} />
+				<Route path='/update-user-info' element={<UpdateUserInfo />} />
+				<Route path='/mypage' element={<ViewMyUserInfo />} />
       </Routes>
       <Footer />
     </div>
