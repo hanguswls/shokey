8cfc566be66907eb7c4f2002fcc5f3799cb11bfb
Jonghyeok <jhpmuscle@gmail.com>
2024-08-07 15:51:42 +0900

    style: 공고 목록 기본 레이아웃

---
+++ src/routes/Posts.jsx
@@ -0,0 +1,75 @@
+import './Posts.css';
+
+function Posts() {
+  return (
+    <main className="posts">
+      <section className="posts-inner">
+        <h2 className="posts-title">공고 둘러보기</h2>
+        <section className="posts-container">
+          <article className="post-card">
+            <figure className="post-card-image">
+              <img src="https://picsum.photos/500.jpg" alt="post" />
+            </figure>
+            <h4 className="post-card-title">다이어트 해주실분</h4>
+            <span className="post-card-user">한현진</span>
+            <p className="post-card-content">
+              다이어트(diet)를 영어로 직역하면, 식단(食單)이라는 뜻을 가진 어휘이며, 
+              그 뜻은 특정한 목적을 위하여 정해 놓는 식사 계획을 이르는 단어이다.[2] 
+              그러나 식량이 풍부해져 기아는 줄어들고, 과식으로 인한 체중증가문제가 사회문제로 
+              대두된 현대인들은 식단 계획을 정상 체중을 유지하기 위한 체중감량을 위해서 하는 
+              경우가 대부분이기 때문에, 현대에 이르로 다이어트의 뜻은 보통 체중을 조절하기 
+              위한 식단(식이 요법)으로 많이 쓰인다.
+            </p>
+          </article>
+          <article className="post-card">
+            <figure className="post-card-image">
+              <img src="https://picsum.photos/500.jpg" alt="post" />
+            </figure>
+            <h4 className="post-card-title">다이어트 해주실분다이어트 해주실분다이어트 해주실분</h4>
+            <span className="post-card-user">한현진</span>
+            <p className="post-card-content">
+              다이어트(diet)를 영어로 직역하면, 식단(食單)이라는 뜻을 가진 어휘이며, 
+              그 뜻은 특정한 목적을 위하여 정해 놓는 식사 계획을 이르는 단어이다.[2] 
+              그러나 식량이 풍부해져 기아는 줄어들고, 과식으로 인한 체중증가문제가 사회문제로 
+              대두된 현대인들은 식단 계획을 정상 체중을 유지하기 위한 체중감량을 위해서 하는 
+              경우가 대부분이기 때문에, 현대에 이르로 다이어트의 뜻은 보통 체중을 조절하기 
+              위한 식단(식이 요법)으로 많이 쓰인다.
+            </p>
+          </article>
+          <article className="post-card">
+            <figure className="post-card-image">
+              <img src="https://picsum.photos/500.jpg" alt="post" />
+            </figure>
+            <h4 className="post-card-title">다이어트 해주실분</h4>
+            <span className="post-card-user">한현진</span>
+            <p className="post-card-content">
+              다이어트(diet)를 영어로 직역하면, 식단(食單)이라는 뜻을 가진 어휘이며, 
+              그 뜻은 특정한 목적을 위하여 정해 놓는 식사 계획을 이르는 단어이다.[2] 
+              그러나 식량이 풍부해져 기아는 줄어들고, 과식으로 인한 체중증가문제가 사회문제로 
+              대두된 현대인들은 식단 계획을 정상 체중을 유지하기 위한 체중감량을 위해서 하는 
+              경우가 대부분이기 때문에, 현대에 이르로 다이어트의 뜻은 보통 체중을 조절하기 
+              위한 식단(식이 요법)으로 많이 쓰인다.
+            </p>
+          </article>
+          <article className="post-card">
+            <figure className="post-card-image">
+              <img src="https://picsum.photos/500.jpg" alt="post" />
+            </figure>
+            <h4 className="post-card-title">다이어트 해주실분</h4>
+            <span className="post-card-user">한현진</span>
+            <p className="post-card-content">
+              다이어트(diet)를 영어로 직역하면, 식단(食單)이라는 뜻을 가진 어휘이며, 
+              그 뜻은 특정한 목적을 위하여 정해 놓는 식사 계획을 이르는 단어이다.[2] 
+              그러나 식량이 풍부해져 기아는 줄어들고, 과식으로 인한 체중증가문제가 사회문제로 
+              대두된 현대인들은 식단 계획을 정상 체중을 유지하기 위한 체중감량을 위해서 하는 
+              경우가 대부분이기 때문에, 현대에 이르로 다이어트의 뜻은 보통 체중을 조절하기 
+              위한 식단(식이 요법)으로 많이 쓰인다.
+            </p>
+          </article>
+        </section>
+      </section>
+    </main>
+  )
+}
+
+export default Posts;
