8234cddaf07a558f340d9960ac1b3469613efd50
hj <hanguswls@knu.ac.kr>
2024-08-09 08:55:09 +0900

    feat: 인플루언서 등록, 수정 페이지

---
+++ src/routes/UpdateInfluencer.jsx
@@ -0,0 +1,78 @@
+import './Influencer.css';
+import useMyInfluencer from '../hooks/useMyInfluencer';
+
+function UpdateInfluencer() {
+  const {
+    myInfluencer,
+    handleChannelIdChange,
+    handleSubscribersChange,
+    handleNicheChange,
+    handleProfileImageChange,
+    handleUpdateInfluencer,
+  } = useMyInfluencer();
+
+  return (
+    <main className='influencer-container'>
+      <h1 className='influencer-title'>인플루언서 정보 수정</h1>
+      <section className='profile-upload'>
+        <figure className='profile-image'>
+          {myInfluencer.profile_image && (
+            <img src={myInfluencer.profile_image} alt='profile preview' />
+          )}
+        </figure>
+        <div className='filebox'>
+          <input className='influencer-field-input'
+            value={myInfluencer.profile_image ? myInfluencer.profile_image : '첨부파일'} 
+            readOnly />
+          <label htmlFor='file'>파일 선택</label>
+          <input
+            type='file'
+            onChange={handleProfileImageChange}
+            id='file'
+          />
+        </div>
+      </section>
+      <section className='influencer-fields'>
+        <div className='influencer-field'>
+          <label htmlFor='channelId' className='influencer-field-name'>채널 ID</label>
+          <input
+            id='channelId'
+            className='influencer-field-input'
+            value={myInfluencer.channelId}
+            onChange={handleChannelIdChange}
+          />
+        </div>
+        <div className='influencer-field'>
+          <label htmlFor='subscribers' className='influencer-field-name'>구독자 수</label>
+          <input
+            id='subscribers'
+            className='influencer-field-input'
+            type='number'
+            min={0}
+            value={myInfluencer.subscribers}
+            onChange={handleSubscribersChange}
+          />
+        </div>
+        <div className='influencer-field'>
+          <label htmlFor='niche' className='influencer-field-name'>분야</label>
+          <select
+            id='niche'
+            className='influencer-field-select'
+            placeholder='분야'
+            onChange={handleNicheChange}
+            required
+          >
+            <option value=''>분야 선택</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+          </select>
+        </div>
+      </section>
+      <button onClick={handleUpdateInfluencer} className='register-btn'>
+        수정 완료
+      </button>
+    </main>
+  );
+}
+
+export default UpdateInfluencer;
