169bc3470f0fb3ffe94a76a3b019577640545209
hj <hanguswls@knu.ac.kr>
2024-08-09 08:59:25 +0900

    feat: 인플루언서 관련 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Login from './routes/Login'
 import useRefresh from './hooks/useRefresh'
 import CreatePost from './routes/CreatePost'
 import Posts from './routes/Posts'
+import ViewMyInfluencer from './routes/ViewMyInfluencer'
+import ViewInfluencer from './routes/ViewInfluencer'
+import RegisterInfluencer from './routes/RegisterInfluencer'
+import UpdateInfluencer from './routes/UpdateInfluencer'
 
 function App() {
 	useRefresh();
@@ -21,6 +25,10 @@ function App() {
 				<Route path='/login' element={<Login />} />
 				<Route path='/posts' element={<Posts />} />
 				<Route path='/upload' element={<CreatePost />} />
+        <Route path='/register-influencer' element={<RegisterInfluencer />}></Route>
+        <Route path='/update-influencer' element={<UpdateInfluencer />}></Route>
+        <Route path='/my-influencer' element={<ViewMyInfluencer />}></Route>
+        <Route path='/influencer/:id' element={<ViewInfluencer />}></Route>
       </Routes>
       <Footer />
     </div>
