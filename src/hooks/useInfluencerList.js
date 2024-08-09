b1924ecac37d172b0c8f6387f550472ab75c0791
Jonghyeok <jhpmuscle@gmail.com>
2024-08-09 11:35:05 +0900

    fix: 인플루언서 목록 페이징 관련 상태 추가

--- src/hooks/useInfluencerList.js
+++ src/hooks/useInfluencerList.js
@@ -3,18 +3,41 @@ import { getInfluencerList } from "../apis/influencerApi";
 
 function useInfluencerList() {
   const [influencerList, setInfluencerList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pagingBtn, setPagingBtn] = useState([]);
 
   useEffect(() => {
-    getInfluencerList(0, 12, [])
+    getInfluencerList(page, 12, [])
     .then((res) => {
       setInfluencerList(res.data.content);
+      setTotalPages(res.data.totalPages);
     })
     .catch((error) => {
       alert(error.message);
     })
-  }, [])
+  }, [page]);
 
-  return { influencerList };
+  useEffect(() => {
+    const startPage = (Math.floor(page / 5) * 5) + 1;
+    const endPage = (startPage + 4) <= totalPages ? (startPage + 4) : totalPages;
+    let pageNums = [];
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageNums.push(i);
+    }
+
+    console.log(pageNums);
+    setPagingBtn(pageNums);
+  }, [page, totalPages]);
+
+  return { 
+    influencerList,
+    page,
+    setPage,
+    totalPages,
+    pagingBtn
+  };
 }
 
 export default useInfluencerList;
