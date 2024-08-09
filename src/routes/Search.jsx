fd0736d71db6b2998c0cb4083737614a30a68bf1
Jonghyeok <jhpmuscle@gmail.com>
2024-08-09 11:31:59 +0900

    style: 필터, 인플루언서 목록 레이아웃

--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -1,7 +1,15 @@
 import './Search.css';
 import searchIcon from '../assets/search_icon.png';
+import filterIcon from '../assets/filter_icon.png';
+import clearIcon  from '../assets/clear_icon.png';
+import InfluencerCard from '../components/card/InfluencerCard';
+import useInfluencerList from '../hooks/useInfluencerList';
 
 function Search() {
+  const {
+    influencerList
+  } = useInfluencerList();
+
   return (
     <main className="search">
       <section className="search-wrapper">
@@ -10,12 +18,39 @@ function Search() {
           <input type="text" name="search" id="search" placeholder='Find the perfect SHOKEYs for your campaign...' />
         </div>
       </section>
-      <aside className="filter">
-        내용
-      </aside>
-      <section className="influencers-container">
-        내용
-      </section>
+      <div className="search-main-content">
+        <aside className="filter">
+          <div className="filter-title-wrapper">
+            <img src={filterIcon} alt="filter-icon" />
+            <h3>Filter</h3>
+            <span>
+              Clear All 
+              <img src={clearIcon} alt="clear-icon" />
+            </span>
+          </div>
+          <article className="filter-niche">
+            <h4>Niche</h4>
+            <ul>
+              <li>Food  (100)</li>
+              <li>Fashion  (100)</li>
+              <li>Travel  (100)</li>
+            </ul>
+          </article>
+        </aside>
+        <section className="influencers-container">
+          {
+            influencerList.map((item, i) => {
+              return <InfluencerCard 
+                        key={i}
+                        userName={item.userInfo.userName}
+                        verified={item.verified}
+                        // profile_image={item.profile_image}
+                        profile_image={"https://picsum.photos/500.jpg?random" + i}
+                        subscribers={item.subscribers} />
+              })
+            }
+        </section>
+      </div>
     </main>
   )
 }
