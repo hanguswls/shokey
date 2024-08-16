28b65d268892116d0cbf9bf0d75320c558fafb4f
Jonghyeok <jhpmuscle@gmail.com>
2024-08-16 12:58:24 +0900

    feat: 글 항목 가져오기

--- src/routes/PostDetail.jsx
+++ src/routes/PostDetail.jsx
@@ -1,23 +1,35 @@
 import { useParams } from 'react-router-dom';
 import './PostDetail.css';
+import { useEffect, useState } from 'react';
+import { getPost } from '../apis/postApi';
 
 function PostDetail() {
   const params = useParams();
-  const description = "도도소미 인스타그램 팔로우후 캠페인 신청을 해주세요~!!   1. 도도소미 인스타그램 팔로우: 도도소미 공식 인스타그램 계정을 팔로우합니다.  2.도도소미 목걸이: 추첨을 통해 도도소미 목걸이를 선물로 드립  3. 태그바이로 꼭 신청을 해주세요!!\n\n'목록'버튼 넣을지말지 고민"
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    getPost(params.id)
+    .then((res) => {
+      setPost(res.data);
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+  }, [params])
 
   return (
     <main className="post-detail">
       <section className="post-detail-contents">
         <article className="title-wrapper">
-          <h3>도도소미</h3>
-          <h1>공고 제목 예시입니다</h1>
+          <h3>{post?.authorName}</h3>
+          <h1>{post?.title}</h1>
         </article>
         <article className="content-wrapper">
           <figure className="post-detail-image">
             <img src="https://picsum.photos/500/300" alt="" />
           </figure>
           <p className="post-detail-description">
-            {description}
+            {post?.content}
           </p>
         </article>
         <article className="post-detail-conditions">
@@ -25,13 +37,13 @@ function PostDetail() {
             <li>
               마감일
               <div>
-                2024-08-16
+                {post?.endDate}
               </div>
             </li>
             <li>
               인센티브
               <div>
-                10,000
+                {Number(post?.extraPrice).toLocaleString()}
                 <span>
                   원
                 </span>
@@ -40,7 +52,7 @@ function PostDetail() {
             <li>
               계약금
               <div>
-                300,000
+                {Number(post?.price).toLocaleString()}
                 <span>
                   원
                 </span>
