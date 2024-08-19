6a84708a325fe228f1ffb719538d72ccd4b04c6a
Jonghyeok <jhpmuscle@gmail.com>
2024-08-19 14:00:24 +0900

    feat: 입력폼 내용 저장

--- src/routes/Apply.jsx
+++ src/routes/Apply.jsx
@@ -1,24 +1,80 @@
 import './Apply.css';
-import { useParams } from "react-router-dom";
+import { useOutlet, useParams } from "react-router-dom";
 import { handleTextareaChange } from "../utils/textareaHandler";
 import plusIcon from '../assets/plus_icon.png';
+import { useEffect, useRef, useState } from 'react';
 
 function Apply() {
+  const input = useRef();
   const params = useParams();
+  const [uploadVideo, setUploadVideo] = useState();
+  const [videoUrl, setVideoUrl] = useState();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (uploadVideo) {
+      const objUrl = URL.createObjectURL(uploadVideo);
+      setVideoUrl(objUrl);
+    }
+  }, [uploadVideo])
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setUploadVideo(file);
+  }
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+    handleTextareaChange(e);
+  }
+
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+
+    if (!title.trim()) {
+      alert('제목을 입력해주세요.');
+      return;
+    }
+
+    if (!description.trim()) {
+      alert('내용을 입력해주세요.');
+      return;
+    }
+
+    if (!uploadVideo) {
+      alert('영상을 추가해주세요.');
+      return;
+    }
+
+    console.log(title,description, uploadVideo);
+  }
 
   return (
     <main className="apply">
       <section className="apply-inner">
         <h1>공고 지원</h1>
-        <input type="text" placeholder="제목"/>
+        <input type="text" placeholder="제목" onChange={handleTitleChange}/>
         <figure>
-          <img src="https://picsum.photos/500.jpg" alt="" />
-          <button>
+          {
+            videoUrl ? <video src={videoUrl} controls={true}/> : 
+              <div>
+                영상을 업로드해주세요.
+              </div>
+          }
+          <button onClick={() => {
+            input.current.click();
+          }}>
             <img src={plusIcon} alt="plus-icon" />
           </button>
+          <input type="file" accept='video/*' ref={input} onChange={handleFileInputChange} />
         </figure>
-        <textarea rows={8} onChange={handleTextareaChange} placeholder="지원 내용"></textarea>
-        <button>
+        <textarea rows={8}placeholder="지원 내용" onChange={handleDescriptionChange}></textarea>
+        <button type='submit' onClick={handleSubmitClick}>
           지원하기
         </button>
       </section>
