f2168b0d9474e6cf6f42f771053c527e2305d8f3
hj <hanguswls@knu.ac.kr>
2024-08-20 15:48:11 +0900

    feat: 헤더에 사용자 정보 반영

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,13 @@ import { useNavigate } from 'react-router-dom';
 import './Header.css';
 import Navbar from "./Navbar";
 import logo from '/logo.png';
+import useLogout from '../../hooks/useLogout';
+import useUserStore from '../../store/useUserStore';
 
 function Header() {
   const navigate = useNavigate();
+  const handleLogout = useLogout();
+  const { user } = useUserStore();
 
   return (
     <header>
@@ -15,12 +19,25 @@ function Header() {
         </span>
         <Navbar />
         <div className="button-container">
-          <button id="login-btn" onClick={() => { navigate('/login') }}>
-            Login
-          </button>
-          <button id="sign-up-btn" onClick={() => { navigate('/register') }}>
-            Sign up
-          </button>
+        { user ? (
+            <>
+              <button id="logout-btn" onClick={handleLogout}>
+                Logout
+              </button>
+              <button id="mypage-btn" onClick={() => {navigate(`/mypage`)}}>
+                {user.userName}님
+              </button>
+            </>
+          ) : (
+            <>
+              <button id="login-btn" onClick={() => { navigate('/login') }}>
+                Login
+              </button>
+              <button id="sign-up-btn" onClick={() => { navigate('/register') }}>
+                Sign up
+              </button>
+            </>
+          )}
         </div>
       </section>
     </header>
