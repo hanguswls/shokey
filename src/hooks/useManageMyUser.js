7f47ffa6b5fec65c05167077695e7e33c10e64d3
hj <hanguswls@knu.ac.kr>
2024-08-21 09:19:44 +0900

    feat: 사용자 정보 수정, 회원 탈퇴 훅

---
+++ src/hooks/useManageMyUser.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import { putUser, deleteUser } from '../apis/userApi';
+import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import useMyUser from './useMyUser';
+import useLogout from './useLogout';
+
+/**
+ * 주요 기능:
+ * - 사용자 정보 수정 (UpdateUser.jsx에서 사용)
+ * - 회원 탈퇴
+ */
+const useManageMyUser = () => {
+  const [cookies, _ , removeCookies] = useCookies(['accessToken', 'refreshToken']);
+  const { user, setUser } = useMyUser();
+  const [updatedUser, setUpdatedUser] = useState();
+  const navigate = useNavigate();
+  const logout = useLogout();
+
+  useEffect(() => {
+    if (user) {
+      setUpdatedUser(user);
+    }
+  }, [user]);
+
+  const handleUserChange = (e) => {
+    const { name, value } = e.target;
+    setUpdatedUser(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleUpdateUser = async () => {
+    try {
+      await putUser(user, cookies.accessToken);
+      setUser(updatedUser);
+      alert('사용자 정보가 수정되었습니다.');
+      navigate('/mypage');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    const isConfirmed = window.confirm('정말로 탈퇴하시겠습니까?');
+    if (!isConfirmed) return;
+    try {
+      await deleteUser(cookies.accessToken);
+      setUser(null);
+      setUpdatedUser(null);
+      removeCookies('accessToken');
+      removeCookies('refreshToken');
+      alert('탈퇴되었습니다.');
+      logout();
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return {
+    updatedUser,
+    handleUserChange,
+    handleUpdateUser,
+    handleDeleteUser,
+  };
+}
+
+export default useManageMyUser
