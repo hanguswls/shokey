b8a1435f5740e5185a07e8905158667e12bdbf5b
hj <hanguswls@knu.ac.kr>
2024-08-23 08:06:39 +0900

    feat: 지원한 공고 리스트 조회

--- src/apis/applyApi.js
+++ src/apis/applyApi.js
@@ -16,4 +16,38 @@ const postApply = async (applyData, postId, token) => {
   return res.json();
 }
 
-export { postApply };
+const getMyApplyList = async (accessToken) => {
+	const res = await fetch(import.meta.env.VITE_APP_API_URL + '/api/applies/my', {
+	  method: 'GET',
+	  credentials: 'include',
+	  headers : {
+		'Authorization': `Bearer ${accessToken}`
+	  },
+	});
+
+  if (!res.ok) {
+    const message = (await res.json()).statusMsg;
+    throw new Error(message);
+  }
+
+  return res.json();
+}
+
+const getApplyList = async (influencerId) => {
+	const res = await fetch(import.meta.env.VITE_APP_API_URL + `/api/applies/${influencerId}`, {
+	  method: 'GET',
+	});
+
+  if (!res.ok) {
+    const message = (await res.json()).statusMsg;
+    throw new Error(message);
+  }
+
+  return res.json();
+}
+
+export {
+  postApply,
+  getMyApplyList,
+  getApplyList,
+};
