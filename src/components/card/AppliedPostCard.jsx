6a76d9a662a96ce20aaae8a726898ff126530435
hj <hanguswls@knu.ac.kr>
2024-08-23 08:05:45 +0900

    feat: 지원한 공고 카드

---
+++ src/components/card/AppliedPostCard.jsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import "./AppliedPostCard.css";
+import acceptedMark from "/public/accepted_mark.png";
+import biddedMark from "/public/bidded_mark.png";
+
+function AppliedPostCard(props) {
+  const navigate = useNavigate();
+  const {
+    title,
+    user,
+    image,
+    id,
+    bidded,
+    accepted
+  } = props;
+
+  return (
+    <article className="applied-post-card" onClick={() => { navigate('/post/' + id) }}>
+      <figure className="applied-post-card-image">
+        { image ? (
+          <img src={image} alt="post" />
+        ) : (
+          <div className="no-image" />
+        )}
+      </figure>
+      <h4 className="applied-post-card-title">{ title }</h4>
+      <span className="applied-post-card-user">{ user }</span>
+      {/* {accepted && ( */}
+        <figure className="accepted-mark">
+          <img src={acceptedMark} alt="accepted-mark" />
+        </figure>
+      {/* )} */}
+      {/* {bidded && ( */}
+        <figure className="bidded-mark">
+          <img src={biddedMark} alt="bidded-mark" />
+        </figure>
+      {/* )} */}
+    </article>
+  );
+}
+
+export default AppliedPostCard;
