1a6701141b88bc596d7e07a4ff079f065f3386a1
Jonghyeok <jhpmuscle@gmail.com>
2024-08-28 14:14:32 +0900

    feat: 유튜브 조회수 조회 및 유튜브 업로드 영상 조회

--- src/components/card/ApplyCard.jsx
+++ src/components/card/ApplyCard.jsx
@@ -3,9 +3,12 @@ import { useCookies } from "react-cookie";
 import { acceptBidApi, registerBidApi } from "../../apis/bidApi";
 import approveTag from '/src/assets/approved_tag.svg';
 import biddedTag from '/src/assets/bidded_tag.svg';
+import { useEffect, useState } from 'react';
+import { getVideoInfoListWithId } from '../../apis/youtubeApi';
 
 function ApplyCard(props) {
   const [ cookies ] = useCookies(['accessToken']);
+  const [ viewCount, setViewCount ] = useState();
   const {
     applyId,
     title,
@@ -37,20 +40,44 @@ function ApplyCard(props) {
     catch (err) { alert(err.message) }
   }
 
+  const getVideoInfoFromServer = async () => {
+    // shortID가 없으면 실행 종료
+    if (!shortsId) return ;
+
+    try {
+      const res = await getVideoInfoListWithId(shortsId);
+      const viewCountData = res.items[0].statistics.viewCount;
+      setViewCount(viewCountData);
+    }
+    catch (err) { alert(err.message) }
+  }
+
+  useEffect(() => {
+    getVideoInfoFromServer();
+  }, [shortsId])
+
   return (
     <article className="apply-card">
       { accepted || bidded ? <img className="apply-tag" src={accepted ? approveTag : biddedTag} alt="tag" /> : null }
       <figure>
-        <video src={ videoLink } controls={true}/>
+        {
+          shortsId ? <iframe src={"//www.youtube.com/embed/" + shortsId}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            /> :
+            <video src={ videoLink } controls={true}/>
+        }
       </figure>
       <span>{ createdAt }</span>
       <h3>{ title }</h3>
       <span>{ influencerName }</span>
+      { viewCount ? <span>{ viewCount }회 시청</span> : null } 
       <p>{ content }</p>
       <div>
         {
-          bidded ? <button onClick={handleClickAcceptButton}>승인하기</button> : 
-          <button onClick={handleClickBidButton}>입찰하기</button>
+          bidded && !accepted ? <button onClick={handleClickAcceptButton}>승인하기</button> :
+          !bidded && !accepted ? <button onClick={handleClickBidButton}>입찰하기</button> :
+          null
         }
       </div>
     </article>
